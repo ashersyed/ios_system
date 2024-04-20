f63a84d7653028049d02c7d7c42ae3deb7d76ca1
Asher Syed <ashersyed@hotmail.com>
2024-04-20 16:53:08 +0500

    fixing getting sources

--- get_sources.sh
+++ get_sources.sh
@@ -17,7 +17,8 @@ rm -rf $file $libutil $libinfo $shell $text $archive $curl
 echo "Getting file_cmds"
 # curl https://opensource.apple.com/tarballs/file_cmds/$file.tar.gz -O
 curl https://codeload.github.com/apple-oss-distributions/file_cmds/tar.gz/refs/tags/$file -o $file.tar.gz
-tar xfz $file.tar.gz
+mkdir $file
+tar xfz $file.tar.gz -C $file --strip 1
 rm $file.tar.gz
 # move to position independent of version number
 # so Xcode project stays valid
@@ -28,8 +29,8 @@ mv $file file_cmds
 echo "Getting libutil"
 # curl https://opensource.apple.com/tarballs/libutil/$libutil.tar.gz -O
 curl https://codeload.github.com/apple-oss-distributions/libutil/tar.gz/refs/tags/$libutil -o $libutil.tar.gz
-
-tar xfz $libutil.tar.gz
+mkdir $libutil
+tar xfz $libutil.tar.gz -C $libutil --strip 1
 rm $libutil.tar.gz
 mv $libutil libutil
 (cd libutil ; patch -p1 < ../libutil.patch ; cd ..)
@@ -38,16 +39,17 @@ mv $libutil libutil
 echo "Getting libinfo"
 # curl https://opensource.apple.com/tarballs/Libinfo/$libinfo.tar.gz -O
 curl https://codeload.github.com/apple-oss-distributions/Libinfo/tar.gz/refs/tags/$libinfo -o $libinfo.tar.gz
-
-tar xfz $libinfo.tar.gz
+mkdir $libinfo
+tar xfz $libinfo.tar.gz -C $libinfo --strip 1
 rm $libinfo.tar.gz
 mv $libinfo libinfo
 
 # get source for shell_cmds:
 echo "Getting shell_cmds"
 # curl https://opensource.apple.com/tarballs/shell_cmds/$shell.tar.gz -O
-curl https://codeload.github.com/apple-oss-distributions/shell_cmds/tar.gz/refs/tags/$shell_cmds -o $shell_cmds.tar.gz
-tar xfz $shell.tar.gz
+curl https://codeload.github.com/apple-oss-distributions/shell_cmds/tar.gz/refs/tags/$shell -o $shell.tar.gz
+mkdir $shell
+tar xfz $shell.tar.gz -C $shell --strip 1
 rm $shell.tar.gz
 mv $shell shell_cmds
 (cd shell_cmds ; patch -p1 < ../shell_cmds.patch ; cd ..)
@@ -55,16 +57,18 @@ mv $shell shell_cmds
 # get source for text_cmds:
 echo "Getting text_cmds"
 # curl https://opensource.apple.com/tarballs/text_cmds/$text.tar.gz -O
-curl https://codeload.github.com/apple-oss-distributions/text_cmds/tar.gz/refs/tags/$text_cmds -o $text_cmds.tar.gz
-tar xfz $text.tar.gz
+curl https://codeload.github.com/apple-oss-distributions/text_cmds/tar.gz/refs/tags/$text -o $text.tar.gz
+mkdir $text
+tar xfz $text.tar.gz -C $text --strip 1
 rm $text.tar.gz
 mv $text text_cmds
 (cd text_cmds ; patch -p1 < ../text_cmds.patch ; cd ..)
 
 # get source for BSD-tar: (not gnu-tar because licensing issues).
 # curl https://opensource.apple.com/tarballs/libarchive/$archive.tar.gz -O
-curl https://codeload.github.com/apple-oss-distributions/libarchive/tar.gz/refs/tags/$libarchive -o $archive.tar.gz
-tar xfz $archive.tar.gz
+curl https://codeload.github.com/apple-oss-distributions/libarchive/tar.gz/refs/tags/$archive -o $archive.tar.gz
+mkdir $archive
+tar xfz $archive.tar.gz -C $archive --strip 1
 rm $archive.tar.gz
 mv $archive libarchive
 (cd libarchive ; patch -p1 < ../libarchive.patch ; cd ..)
@@ -72,7 +76,8 @@ mv $archive libarchive
 # get source for curl. This one requires OpenSSH + libssl
 # curl https://opensource.apple.com/tarballs/curl/$curl.tar.gz -O
 curl https://codeload.github.com/apple-oss-distributions/curl/tar.gz/refs/tags/$curl -o $curl.tar.gz
-tar xfz $curl.tar.gz
+mkdir $curl
+tar xfz $curl.tar.gz -C $curl --strip 1
 rm $curl.tar.gz
 mv $curl curl
 (cd curl ; patch -p1 < ../curl.patch ; cd ..)
--- get_sources_for_patching.sh
+++ get_sources_for_patching.sh
@@ -1,65 +1,82 @@
 #! /bin/sh
 
 # edit for latest version numbers:
-file=file_cmds-272
-libutil=libutil-51
-libinfo=Libinfo-517
-shell=shell_cmds-203
-text=text_cmds-99
-archive=libarchive-54
-curl=curl-105
+file=file_cmds-430.100.5
+libutil=libutil-72
+libinfo=Libinfo-583.0.1
+shell=shell_cmds-309
+text=text_cmds-165.100.8
+archive=libarchive-131
+curl=curl-154
 
 find . -name .DS_Store -exec rm {} \; -print
 
 # get source for file_cmds
 echo "Getting file_cmds"
-curl https://opensource.apple.com/tarballs/file_cmds/$file.tar.gz -O
-tar xfz $file.tar.gz
+# curl https://opensource.apple.com/tarballs/file_cmds/$file.tar.gz -O
+curl https://codeload.github.com/apple-oss-distributions/file_cmds/tar.gz/refs/tags/$file -o $file.tar.gz
+mkdir $file
+tar xfz $file.tar.gz -C $file --strip 1
 rm $file.tar.gz
+
 echo "Creating file_cmds.patch"
 diff -Naur $file file_cmds > file_cmds.patch
 rm -rf $file
 
-
+# get source for libutil:
 echo "Getting libutil"
-curl https://opensource.apple.com/tarballs/libutil/$libutil.tar.gz -O
-tar xfz $libutil.tar.gz
+# curl https://opensource.apple.com/tarballs/libutil/$libutil.tar.gz -O
+curl https://codeload.github.com/apple-oss-distributions/libutil/tar.gz/refs/tags/$libutil -o $libutil.tar.gz
+mkdir $libutil
+tar xfz $libutil.tar.gz -C $libutil --strip 1
 rm $libutil.tar.gz
+
 echo "Creating libutil.patch"
 diff -Naur $libutil libutil > libutil.patch
 rm -rf $libutil
 
-
 # get source for shell_cmds:
 echo "Getting shell_cmds"
-curl https://opensource.apple.com/tarballs/shell_cmds/$shell.tar.gz -O
-tar xfz $shell.tar.gz
+# curl https://opensource.apple.com/tarballs/shell_cmds/$shell.tar.gz -O
+curl https://codeload.github.com/apple-oss-distributions/shell_cmds/tar.gz/refs/tags/$shell -o $shell.tar.gz
+mkdir $shell
+tar xfz $shell.tar.gz -C $shell --strip 1
 rm $shell.tar.gz
+
 echo "Creating shell_cmds.patch"
 diff -Naur $shell shell_cmds > shell_cmds.patch
 rm -rf $shell
 
 # get source for text_cmds:
 echo "Getting text_cmds"
-curl https://opensource.apple.com/tarballs/text_cmds/$text.tar.gz -O
-tar xfz $text.tar.gz
-rm $text.tar.gz 
+# curl https://opensource.apple.com/tarballs/text_cmds/$text.tar.gz -O
+curl https://codeload.github.com/apple-oss-distributions/text_cmds/tar.gz/refs/tags/$text -o $text.tar.gz
+mkdir $text
+tar xfz $text.tar.gz -C $text --strip 1
+rm $text.tar.gz
+
 echo "Creating text_cmds.patch"
 diff -Naur $text text_cmds > text_cmds.patch
 rm -rf $text
 
 # get source for BSD-tar: (not gnu-tar because licensing issues).
-curl https://opensource.apple.com/tarballs/libarchive/$archive.tar.gz -O
-tar xfz $archive.tar.gz
+# curl https://opensource.apple.com/tarballs/libarchive/$archive.tar.gz -O
+curl https://codeload.github.com/apple-oss-distributions/libarchive/tar.gz/refs/tags/$archive -o $archive.tar.gz
+mkdir $archive
+tar xfz $archive.tar.gz -C $archive --strip 1
 rm $archive.tar.gz
+
 echo "Creating libarchive.patch"
 diff -Naur $archive libarchive > libarchive.patch
 rm -rf $archive
 
 # get source for curl. This one requires OpenSSH + libssl
-curl https://opensource.apple.com/tarballs/curl/$curl.tar.gz -O
-tar xfz $curl.tar.gz
+# curl https://opensource.apple.com/tarballs/curl/$curl.tar.gz -O
+curl https://codeload.github.com/apple-oss-distributions/curl/tar.gz/refs/tags/$curl -o $curl.tar.gz
+mkdir $curl
+tar xfz $curl.tar.gz -C $curl --strip 1
 rm $curl.tar.gz
+
 echo "Creating curl.patch"
 diff -Naur $curl curl > curl.patch
 rm -rf $curl
